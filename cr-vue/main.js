6e62b5de3f81890430dc8e95923af4f21c0a38bf
tuka <tukaelu@hatena.ne.jp>
2020-04-16 08:51:19 +0900

    STEP2 ローカルストレージ API の使用 実装

--- cr-vue/main.js
+++ cr-vue/main.js
@@ -9,3 +9,21 @@ const app = new Vue({
     //
   }
 })
+
+// STEP2
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todo.length
+    return tudos
+  },
+  save: function(tods) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
