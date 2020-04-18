28caaacc05763810503c717d2f1a37515417647a
tuka <tukaelu@hatena.ne.jp>
2020-04-18 14:15:23 +0900

    refactor

--- cr-vue/main.js
+++ cr-vue/main.js
@@ -1,3 +1,21 @@
+// STEP2
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
 // STEP1
 const app = new Vue({
   el: '#app',
@@ -37,21 +55,3 @@ const app = new Vue({
     this.todos = todoStorage.fetch()
   }
 })
-
-// STEP2
-var STORAGE_KEY = 'todos-vuejs-demo'
-var todoStorage = {
-  fetch: function() {
-    var todos = JSON.parse(
-      localStorage.getItem(STORAGE_KEY) || '[]'
-    )
-    todos.forEach(function(todo, index) {
-      todo.id = index
-    })
-    todoStorage.uid = todos.length
-    return todos
-  },
-  save: function(todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
-  }
-}
